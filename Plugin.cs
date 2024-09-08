d98b50858bb7691fd910d444eb944aaa0fe196ea
NTkernel <ntkernel1@gmail.com>
2024-09-08 15:50:50 +0100

    1.0

---
+++ Plugin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using BepInEx;
+
+[BepInPlugin("com.ntkernel.PlayTimeroulette", "PlayTimeroulette", "1.0.0")]
+public class timeroulette : BaseUnityPlugin
+{
+    private float roulette;
+
+    void Start()
+    {
+        
+        roulette = Random.Range(0f, 7200f);
+        Logger.LogInfo($"Bye Bye in {roulette} seconds!");
+
+        StartCoroutine(timetogo(roulette));
+    }
+
+    private IEnumerator timetogo(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        Logger.LogInfo("Its time.");
+        Application.Quit(); 
+    }
+}
+
